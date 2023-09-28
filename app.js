fff64de14c6e32cf955f7cd3e793b0f87a1935fe
Agar2002 <agarvin2002@gmail.com>
2023-09-28 10:34:13 -0400

    working on passport.js

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt=require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const app=express();
-
-mongoose.connect("mongodb://localhost:27017/userDB");
-
-const userSchema= new mongoose.Schema({
-    email: String,
-    password: String
-});
-
-console.log(process.env.API_KEY);
-
-
-
-const User=new mongoose.model("User", userSchema);
-
-
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(session({
+    secret:"Our little secret.",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+
+const userSchema= new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User=new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/",function(req,res){
     res.render("home");
 });
