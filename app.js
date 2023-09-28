2670cb96d8d12c10a800bea2d2844f374b15516f
Agar2002 <agarvin2002@gmail.com>
2023-09-28 11:51:36 -0400

    added passport.js for authentication

--- app.js
+++ app.js
@@ -49,48 +49,38 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register", function(req, res) {
-   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-        email: req.body.username,
-        password:hash
-        });
-    newUser.save()
-        .then(() => {
-            res.render("secrets");
-        })
-        .catch(err => {
-            console.error(err);
-        });
+    User.register({username: req.body.username},req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.render("secrets");
+            });
+        }
     });
+
 });
 
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    User.findOne({ email: username })
-        .then(function(foundUser) {
-            if (foundUser.email==username){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result == true){
-                        res.render("secrets");
-                    }
-                });
-            } else {
-                // Handle incorrect username or password
-                res.send("Incorrect username or password");
-            }
-        })
-        .catch(function(err) {
-            console.error(err);
-            res.send("An error occurred");
-        });
+    const user=new User({
+        username: req.body.username,
+        username: req.body.password
+    });
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.render("secrets");
+            });
+        }
+    })
 });
 
 app.get("/logout", function(req,res){
     res.redirect("/");
 });
-
 
 app.listen(3000,function(){
     console.log("Server started on port 3000.");
